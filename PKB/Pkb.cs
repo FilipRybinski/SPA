6db12103d09bf2271c1a625e77299a8c8c1d5329
Filip Rybiński <rybciaw77@gmail.com>
2024-05-26 21:53:16 +0200

    PKB

---
+++ PKB/Pkb.cs
@@ -0,0 +1,33 @@
+﻿using Parser.Interfaces;
+using Parser.Tables;
+using PKB.Interfaces;
+
+namespace PKB;
+
+public class Pkb : IPkb
+{
+    private static Pkb? _singletonInstance;
+    public IUses? Uses => Parser.Uses.Uses.Instance;
+
+    public ICalls? Calls => Parser.Calls.Calls.Instance;
+
+    public IModifies? Modifies => Parser.Modifies.Modifies.Instance;
+
+    public IProcTable? ProcTable => ProcedureTable.Instance;
+
+    public IStmtTable? StmtTable => StatementTable.Instance;
+
+    public IVarTable? VarTable => ViariableTable.Instance;
+
+    public static IPkb? Instance 
+    {
+        get
+        {
+            if (_singletonInstance == null)
+            {
+                _singletonInstance = new Pkb();
+            }
+            return _singletonInstance;
+        }
+    }
+}
