7f570c782a0c32587f91e6630de58c3ae22a532b
Filip Rybiński <rybciaw77@gmail.com>
2024-05-26 22:39:42 +0200

    Add tests

---
+++ SPATests/Utils/Utils.cs
@@ -0,0 +1,30 @@
+using System.Text.RegularExpressions;
+
+namespace SPATests.Utils;
+
+public static class Utils
+{
+    public static string PrepareResults(IReadOnlyCollection<string> results)
+    {
+        if (results.Count == 0)
+        {
+            return "none";
+        }
+
+        return string.Join(", ", results);
+    }
+        
+    public static string PrepareSimpleCode(string filePath)
+    {
+        var code = File.ReadAllText(filePath);
+        code = Regex.Replace(code, @"\r", "");
+        return code;
+    }
+
+    public static void ParseCode(string code)
+    {
+        var parser = new Parser.Parser();
+        parser.CleanData();
+        parser.StartParse(code);
+    }
+}
